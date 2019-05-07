24fa832b416697c5fa673fe8b573ebf789e0edda
Debian Live user <user@localhost.localdomain>
2019-05-07 11:38:56 +0000

    brain front end

---
+++ app.js
@@ -0,0 +1,12 @@
+function myMove() {
+  var elem = document.getElementById(".navbar a:hover");   
+  var pos = 0;
+  var id = setInterval(frame, 2);
+  function frame() {
+    if (pos == 20) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + "px"; 
+      elem.style.left = pos + "px"; 
+    }
