4fd3e95e5bb718ea7e01ad0745da4b79336a5e26
Debian Live user <user@localhost.localdomain>
2019-05-08 13:42:52 +0000

    mobile responsive

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 function myMove() {
-  var elem = document.getElementById(".navbar a:hover");   
+  var elem = document.getElementById('.navbar a:hover');
   var pos = 0;
   var id = setInterval(frame, 2);
   function frame() {
     if (pos == 20) {
       clearInterval(id);
     } else {
-      pos++; 
-      elem.style.top = pos + "px"; 
-      elem.style.left = pos + "px"; 
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
     }
+  }
+}
